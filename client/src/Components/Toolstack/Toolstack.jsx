01cce1e0c1633210bbd433d57480f954a0f70dbf
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2025-03-02 14:47:33 +0530

    Update Toolstack.jsx

--- client/src/Components/Toolstack/Toolstack.jsx
+++ client/src/Components/Toolstack/Toolstack.jsx
@@ -10,7 +10,7 @@ import {
 function Toolstack() {
   return (
     <div className="w-full h-full">
-      <h2 className="text-3xl font-bold text-center mb-4 mt-4"><span className="text-blue-500">Tool </span> I use</h2>
+      <h2 className="text-3xl font-bold text-center mb-4 mt-4"><span className="text-blue-500">Tools </span></h2>
       <div className="flex justify-center items-center pb-10">
         <div className="w-24 h-24 flex items-center justify-center bg-gray-100 rounded-full m-2">
           <SiVisualstudiocode className="text-5xl text-blue-500" />
